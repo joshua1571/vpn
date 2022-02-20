9f6dd4e150af658ac3f34ef414390823ced7ee4c
Joshua Hernandez <joshua1571@users.noreply.github.com>
2022-02-19 19:34:23 -0800

    Added VPC

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,9 +14,19 @@ provider "aws" {
   region  = "us-west-1"
 }
 
+resource "aws_vpc" "vpn_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "VPNVPC"
+  }
+}
+
 resource "aws_instance" "vpn_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  associate_public_ip_address = true
 
   tags = {
     Name = "VPNServer"
